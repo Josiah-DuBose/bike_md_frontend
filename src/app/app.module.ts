92f813ccefda63c13c1aedd0e1173ce1d4bdc6c5
Josiah DuBose <josiah7686@gmail.com>
2019-01-29 14:58:48 -0500

    added nav-bar, index components and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { IndexComponent } from './components/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
