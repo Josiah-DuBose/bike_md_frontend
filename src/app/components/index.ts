6744eae63be00e10cb3a1bc8878ebf9ee9a7a9e5
Josiah DuBose <josiah7686@gmail.com>
2019-12-09 08:43:48 -0500

    clean up

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,17 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IndexComponent } from './index/index.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { AboutComponent } from './about/about.component';
 
 @NgModule({
-	declarations: [
-		IndexComponent,
-		LoginComponent,
-		RegisterComponent,
-		AboutComponent
-	],
+	declarations: [],
 	imports: [
 		CommonModule
 	]
